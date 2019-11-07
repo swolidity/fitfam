8d7d1b870c8818957e16e3d5b571d2c30ca2fe38
Andy Kay <andykay.me@gmail.com>
2019-11-07 10:49:13 -0500

    new logo 💪

--- www/pages/index.js
+++ www/pages/index.js
@@ -10,7 +10,7 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <Flex px={6} py={4} borderBottom="2px" borderBottomColor="#fafafa">
+    <Flex px={6} py={4} backgroundColor="#0D6CFF">
       <Box>
         <Image src="/fitfam@2x.png" alt="FitFam" height="45px" />
       </Box>
